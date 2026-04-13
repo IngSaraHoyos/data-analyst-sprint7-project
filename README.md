0376eb2ffe6b0b59f3deab10ace9a70ce0db8f39
SaraHoyos <fuerzavital.co@gmail.com>
2026-04-13 15:21:56 -0500

    Project additional challenge

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ In this project I have been practicing the skills I
 gathered during this course.
 It basically reads data from a CSV and build an Histogram u
 using Plotly.
+
+Now the project shows some checkboxes asking for the specific
+type of graph the user can generate using one single button,
+as the project additional challenge requires.
--- app.py
+++ app.py
@@ -2,17 +2,29 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.header('Proyecto Sprint 7')
+st.write('Por favor marca las casillas de los tipos de gráficos quieres generar.')
 
 car_data = pd.read_csv('vehicles_us.csv')
 
-hist_button = st.button('Construir histograma')
+hist_selection = st.checkbox('Construir histograma')
+scatter_selection = st.checkbox('Construir gráfico de dispersión')
 
-if hist_button:
+button_generate = st.button('Generar')
+
+if button_generate:
+
+    if hist_selection:
     
-    st.write('Histograma para el conjunto de datos de anuncios de venta de coches')
+        st.write('Histograma para el conjunto de datos de anuncios de venta de coches')
 
-    fig = px.histogram(car_data, x="odometer")
+        fig = px.histogram(car_data, x="odometer")
 
-    st.plotly_chart(fig, width='stretch')
+        st.plotly_chart(fig, width='stretch')
     
+    if scatter_selection:
+    
+        st.write('Gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+
+        fig2 = px.scatter(car_data, x="odometer")
+
+        st.plotly_chart(fig2, width='stretch')
