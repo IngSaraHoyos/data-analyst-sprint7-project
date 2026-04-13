3dd838ad04089aafe2c3193e55d3be66cb8a2823
SaraHoyos <fuerzavital.co@gmail.com>
2026-04-13 15:05:00 -0500

    Project finished and working

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # data-analyst-sprint7-project
 This is the middle course project. (Sprint7)
+
+In this project I have been practicing the skills I
+gathered during this course.
+It basically reads data from a CSV and build an Histogram u
+using Plotly.
--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
 
 st.header('Proyecto Sprint 7')
 
-st.write('Esta aplicación aún no es funcional. En construcción.')
+car_data = pd.read_csv('vehicles_us.csv')
+
+hist_button = st.button('Construir histograma')
+
+if hist_button:
+    
+    st.write('Histograma para el conjunto de datos de anuncios de venta de coches')
+
+    fig = px.histogram(car_data, x="odometer")
+
+    st.plotly_chart(fig, width='stretch')
+    
